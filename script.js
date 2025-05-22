b7225fa6aa5ebb85e5f6b3b82c203010fac00ca1
Marcello080221 <bachratymarci@gmail.com>
2025-05-22 10:54:01 +0200

    done

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 //code of the game
 
 const solution = Math.floor(Math.random() * 100) + 1
+
+const guessEl = document.getElementById("guess")
+
+function TippClicked(){
+    let guess = guessEl.value * 1
+
+    if(guess < solution){
+        alert("📉")
+    }
+    else if(guess > solution){
+        alert("📈")
+    }
+    if(guess = solution){
+        alert("🎉")
+    }
+}
